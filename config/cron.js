a47e1fce193a2d86ddf4bb8d980c2ef11495da35
PHNTOMXD <phantomarcanexd@gmail.com>
2025-07-15 18:50:36 +0545

    Update cron.js

--- config/cron.js
+++ config/cron.js
@@ -4,8 +4,6 @@ import https from "https";
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("GET request sent successfully");
-      else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
